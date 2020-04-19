45cbee7d3c5f377f7ab7fea82aa9c7f0f58f9360
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-04-19 12:42:38 +0200

    Simple function worksheet

---
+++ Functions/Main.py
@@ -0,0 +1,73 @@
+# # Void function <- when the function doesn't return anything
+# # Takes no parameters () <- empty brackets
+
+
+def print_my_name():
+    print("Hello from print_my_name")
+    print("Julia")
+    # print_name("Jack")
+
+
+def print_name(name):
+    print("Hello from print_name")
+    print(name)
+
+
+def print_name_default(name="Julia"):
+    print("Hello from print_name_default")
+    print(name)
+
+
+# # Calling a function
+
+
+print_my_name()
+# print_name("Julia")
+# print()
+# print_name_default()
+# print_name_default("Bob")
+
+# # TODO: Create a function that will print your favorite food item. It will take no parameters and return nothing
+# # TODO: Create a function tat will take your favorite food item and prints it to the console
+# # TODO: Remember to call your functions. They are not your high school friends.
+
+bestNumber = 8
+
+# # variable out of the scope
+# # you create new bestNumber here and initialize it with 10
+
+# def favorite_number():
+#     bestNumber = 10
+#     print(bestNumber)
+#
+#
+# favorite_number()
+#
+# print(bestNumber)
+#
+#
+# def favorite_number_pass_value(number):
+#     print(number)
+#
+#
+# favorite_number_pass_value(bestNumber)
+# print(bestNumber)
+#
+#
+# def change_best_number(number):
+#     return number + 2
+#
+#
+# print("Before change_best_number: ", bestNumber)
+# bestNumber = change_best_number(bestNumber)
+# print("After change_best_number: ", bestNumber)
+
+# # TODO: Create function add that will take two numbers adds them together and returns the result
+
+# # TODO: Big challenge: Create simple program that will act like calculator.
+
+
+# # Create 2 functions add and subtract and one helper function that will take 2 numbers and an operator and decides
+# if it should call add function or subtract based on the passed operator
+# then return the result and print output to the console
+# HINT: <- I will provide if wanted
