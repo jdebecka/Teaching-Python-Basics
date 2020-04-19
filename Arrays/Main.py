160b5064786754a69d99633084949c6a546327e4
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-04-19 10:37:41 +0200

    Array worksheet

---
+++ Arrays/Main.py
@@ -0,0 +1,78 @@
+# # One dimensional array
+array = [1, 3, 5, 2]
+arrayNames = ["Julia", "Jack", "Bob"]
+#
+# # Length of an array
+lenNumbers = len(array)
+lenNames = len(arrayNames)
+
+print('Length of an array of numbers: ', lenNumbers)
+print('Length of an array of names: ', lenNames)
+
+# # Index of an array
+# TODO: What is going to be an output? print(arrayNames[2])
+
+# # Adding items to array
+# array.append(7)
+# print(array)
+
+# # Sorting an array
+# array.sort()
+# print(array)
+# array.reverse()
+# print(array)
+
+# # Challenge remove last item from the array
+
+# TODO: Remove last element from the array
+
+# for i in range(0, len(array)):
+#     print("array of numbers: ", array[i])
+#     # print(arrayNames[i])
+#
+# for i in range(0, len(arrayNames)):
+#     print("array of numbers: ", array[i])
+#     # print(arrayNames[i])
+#
+# for item in array:
+#     print(item)
+# print(sumNumbers / len(array))
+#     # print(arrayNames[??????])
+
+# Challenge
+# Make a simple program to calculate an average of the array with numbers and print it to the console
+
+# TODO: Calculate average of an array named array <- stupid naming by: Julia
+
+# # Two dimensional arrays
+coordinates = [[1, 2], [3, 4], [5, 3]]
+
+# # Question
+# # what's going to be an output of len
+
+# TODO: Print length of and array named coordinates
+
+# For each loop
+# for item in coordinates:
+#     print(item)
+
+# for item in coordinates:
+# print("Hello from the outer loop")
+#     for innerItem in item:
+# print('\033[1m', "Hello from the inner loop", '\033[0m')
+#         print("For Each loop individual item:", innerItem)
+#
+# # Loops with access by index number
+# # Not recommend to use unless you know how many items there are in inner array
+# for i in range(0, len(coordinates)):
+#     print(coordinates[i][0], coordinates[i][1])
+#
+# for i in range(0, len(coordinates)):
+#     print("Hello from the outer loop")
+#     for j in range(0, len(coordinates[i])):
+#         print('\033[1m', "Hello from the inner loop", '\033[0m')
+#         print("Access to individual item: ", coordinates[i][j])
+
+# TODO: Calculate an average of every x, y <- I actually made a mistake when I was first solving this task
+
+# TODO: Modify your average calculator so it prints only average higher than 2.0
